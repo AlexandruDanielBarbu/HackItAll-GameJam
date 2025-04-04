40db81f1505c8f4ca522fdb087c35919a36d33d1
Stefan1627 <stefancalmac@gmail.com>
2025-04-04 20:04:22 +0300

    Added Decision_template
    A panel template for the decision phase of the game

---
+++ Regatul Umbrelor/Assets/Scripts/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Timer : MonoBehaviour
+    {
+
+        public TMPro.TextMeshProUGUI timerText;
+        public float countdownTime = 10f;
+        bool timerRunning = true;
+
+        void Update()
+        {
+            if (timerRunning)
+            {
+                countdownTime -= Time.deltaTime;
+
+                if (countdownTime <= 0f)
+                {
+                    countdownTime = 0f;
+                    timerRunning = false;
+                    timerText.text = "0";
+                    Debug.Log("Countdown-ul s-a oprit!");
+                }
+                else
+                {
+                    int seconds = Mathf.FloorToInt(countdownTime % 60f);
+                    timerText.text = string.Format("{0}", seconds);
+                }
+            }
+        }
+    }
+}
