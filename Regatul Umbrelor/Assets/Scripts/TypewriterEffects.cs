0110af360c58468bc64327f3854496afc6f9acb2
AlexandruDaniel <barbualex.daniel2004@gmail.com>
2025-04-06 09:27:54 +0300

    I have finished the dialog with the noble men.
    Signed-off-by: AlexandruDaniel <barbualex.daniel2004@gmail.com>

--- Regatul Umbrelor/Assets/Scripts/TypewriterEffects.cs
+++ Regatul Umbrelor/Assets/Scripts/TypewriterEffects.cs
@@ -1,42 +1,42 @@
-using UnityEngine;
-using TMPro;
-using System.Collections;
+//using UnityEngine;
+//using TMPro;
+//using System.Collections;
 
-public class TypewriterEffect : MonoBehaviour
-{
-    [Tooltip("Time delay between each character (in seconds).")]
-    public float characterDelay = 0.05f;
+//public class TypewriterEffect : MonoBehaviour
+//{
+//    [Tooltip("Time delay between each character (in seconds).")]
+//    public float characterDelay = 0.05f;
 
-    private bool skip;  // Flag to check if the user wants to skip
-    private bool isTyping;  // Is typing in progress
+//    private bool skip;  // Flag to check if the user wants to skip
+//    private bool isTyping;  // Is typing in progress
 
-    // Coroutine to show text on a given target TMP_Text
-    public IEnumerator ShowText(string textToDisplay, TextMeshProUGUI targetText)
-    {
-        isTyping = true;
-        skip = false;
-        targetText.text = "";
+//    // Coroutine to show text on a given target TMP_Text
+//    public IEnumerator ShowText(string textToDisplay, TextMeshProUGUI targetText)
+//    {
+//        isTyping = true;
+//        skip = false;
+//        targetText.text = "";
 
-        foreach (char c in textToDisplay)
-        {
-            if (skip)
-            {
-                targetText.text = textToDisplay;
-                break;
-            }
-            targetText.text += c;
-            yield return new WaitForSeconds(characterDelay);
-        }
-        isTyping = false;
-    }
+//        foreach (char c in textToDisplay)
+//        {
+//            if (skip)
+//            {
+//                targetText.text = textToDisplay;
+//                break;
+//            }
+//            targetText.text += c;
+//            yield return new WaitForSeconds(characterDelay);
+//        }
+//        isTyping = false;
+//    }
 
-    public void Skip()
-    {
-        skip = true;
-    }
+//    public void Skip()
+//    {
+//        skip = true;
+//    }
 
-    public bool IsComplete()
-    {
-        return !isTyping;
-    }
-}
+//    public bool IsComplete()
+//    {
+//        return !isTyping;
+//    }
+//}
